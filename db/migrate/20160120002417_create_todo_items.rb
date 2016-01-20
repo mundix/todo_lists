63636378d7f11e9123148329474890af1acbabbe
Edmundo Pichardo <ce.pichardo@gmail.com>
2016-01-19 20:26:41 -0400

    Se genero el modelo de todo_item, y se hiso la relacion del todo_lists con el todo_item has_many :todo_items, ademas se le agrego una dependencia :destroy

---
+++ db/migrate/20160120002417_create_todo_items.rb
@@ -0,0 +1,14 @@
+class CreateTodoItems < ActiveRecord::Migration
+  def change
+    create_table :todo_items do |t|
+      t.date :due_date
+      t.string :title
+      t.text :description
+      t.boolean :complete
+      t.references :todo_list, index: true
+
+      t.timestamps null: false
+    end
+    add_foreign_key :todo_items, :todo_lists
+  end
+end
