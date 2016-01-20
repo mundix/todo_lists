048f95719cc4b4de2470fd40f3f99924eace70c3
Edmundo Pichardo <ce.pichardo@gmail.com>
2016-01-19 20:11:30 -0400

    Se arreglo la migracion del todo_list, el nombre al generarse estaba mal todolists, debia sera todo_lists

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160119232015) do
+ActiveRecord::Schema.define(version: 20160120000959) do
 
   create_table "profiles", force: :cascade do |t|
     t.string   "gender"
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 20160119232015) do
 
   add_index "profiles", ["user_id"], name: "index_profiles_on_user_id"
 
-  create_table "todolists", force: :cascade do |t|
+  create_table "todo_lists", force: :cascade do |t|
     t.string   "list_name"
     t.date     "list_due_date"
     t.datetime "created_at",    null: false
